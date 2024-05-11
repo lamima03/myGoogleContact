39db9f53ba1a58b6b7de0ec4d260622f3e17a515
jemima mukanza <jemimukanza@gmail.com>
2024-05-11 18:35:21 +0100

    my first commit with js

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-const header = document.getElementsById(header)
+const hideSideBar = (target) => {
+    let cible = document.getElementById(target);
+    const isHide = cible.classList.contains('hideSection');
+    if (isHide) {
+        cible.classList.remove("hideSection");
+    }
+    else {
+        cible.classList.add("hideSection");
+    }
+}
 
+let form = document.getElementById('form');
+const inputs = form.elements
 
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(inputs.name);
+})
 
+const showNewContactSection= (target)=> {
+    const element = document.getElementById('rightSection').firstElementChild;
+    element.classList.add("hideSection");
+    const cible = document.getElementById(target);
+    cible.classList.remove('hideNewContactSection');
+    cible.classList.add('newcontact');
+}
